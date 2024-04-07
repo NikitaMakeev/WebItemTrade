41cf535b6060523448add2929404a3e66ca09bd9
Nikita Zhuravljov <131643784+NikitaZhuravljov@users.noreply.github.com>
2024-04-07 23:57:43 +0300

    image in product

---
+++ WebItemTrade/WebItemTrade/webTrading/src/main/java/com/web_trade/entity/User.java
@@ -0,0 +1,49 @@
+package com.web_trade.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true, length = 45)
+	private String email;
+
+	@Column(nullable = false, length = 64)
+	private String password;
+
+	@Column(name = "username", nullable = false, length = 20)
+	private String username;
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	
+	
+}
